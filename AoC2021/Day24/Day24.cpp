08d102ef05fbc2d01e600ab8e7b5989747517548
Michael Braedley <mike.braedley@gmail.com>
2021-12-25 15:31:18 -0400

    Day 24 complete (with some trial and error)

--- AoC2021/Day24/Day24.cpp
+++ AoC2021/Day24/Day24.cpp
@@ -30,218 +30,137 @@ int main()
 		program.back().push_back(line);
 	}
 
-	std::array<int, 14> testVal = { 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9 };
+	std::array<int, 14> coefficients = { 6, 9, 9, 1, 4, 1, 9, 9, 9, 1, 1, 1, 1, 1 };
+	int i;
 
-	auto testFront = [&]()
-	{
-		for (int i = 9; i > 0; i--)
-		{
-			for (int j = 9; j > 0; j--)
-			{
-				std::size_t n = 0;
-				ALU alu([&]() -> std::int64_t
-					{
-						if (n == 0)
-						{
-							return i;
-						}
-
-						if (n == 1)
-						{
-							return j;
-						}
-
-						return testVal[n];
-					});
-
-				for (n = 0; n < 3; n++)
-				{
-					for (auto& inst : program[n])
-					{
-						alu.RunInstruction(inst);
-					}
-				}
-
-				if (alu.GetValue(ALU::RegisterName::z) == 0)
-				{
-					std::cout << "i: " << i << " j: " << j <<  std::endl;
-								
-					testVal[0] = i;
-					testVal[1] = j;
-
-					return;
-				}
-			}
-		}
-	};
-
-	testFront();
-
-	std::cout << std::endl;
-
-	/*for (int i = 9; i > 0; i--)
-	{
-		for (int j = 9; j > 0; j--)
+	auto testToMax = [&](int coeff) {
+		std::int64_t minOutput = std::numeric_limits<std::int64_t>::max();
+		for (int nc = 9; nc > 0; nc--)
 		{
 			ALU alu([&]() -> std::int64_t
 				{
-					if (n == 3)
+					if (i == coeff)
 					{
-						return i;
+						return nc;
 					}
-
-					if (n == 4)
-					{
-						return j;
-					}
-
-					return testVal[n];
+					return coefficients[i];
 				});
 
-			for (n = 0; n < 5; n++)
+			for (i = 0; i <= coeff; i++)
 			{
 				for (auto& inst : program[i])
 				{
 					alu.RunInstruction(inst);
 				}
 			}
-
-			if (alu.GetValue(ALU::RegisterName::z) == 0)
+			auto intermediateVal = alu.GetValue(ALU::RegisterName::z);
+			if (intermediateVal < minOutput)
 			{
-				std::cout << "i: " << i << " j: " << j << std::endl;
-				if (i > testVal[3])
-				{
-					testVal[3] = i;
-					testVal[4] = j;
-				}
-				else if (i == testVal[3])
-				{
-					if (j > testVal[4])
-					{
-						testVal[4] = j;
-					}
-				}
+				minOutput = intermediateVal;
+				coefficients[coeff] = nc;
+
+				std::cout << coeff << ": " << minOutput << " " << nc << std::endl;
 			}
 		}
-	}
-
-	std::cout << std::endl;*/
-
-	//for (int i = 9; i > 0; i--)
-	//{
-	//	for (int j = 9; j > 0; j--)
-	//	{
-	//		ALU alu([&]() -> std::int64_t
-	//			{
-	//				if (n == 5)
-	//				{
-	//					return i;
-	//				}
-
-	//				if (n == 6)
-	//				{
-	//					return j;
-	//				}
-
-	//				return testVal[n];
-	//			});
-
-	//		for (n = 0; n < 7; n++)
-	//		{
-	//			for (auto& inst : program[i])
-	//			{
-	//				alu.RunInstruction(inst);
-	//			}
-	//		}
-
-	//		if (alu.GetValue(ALU::RegisterName::z) == 0)
-	//		{
-	//			std::cout << "i: " << i << " j: " << j << std::endl;
-	//			if (i > testVal[5])
-	//			{
-	//				testVal[5] = i;
-	//				testVal[6] = j;
-	//			}
-	//			else if (i == testVal[5])
-	//			{
-	//				if (j > testVal[6])
-	//				{
-	//					testVal[6] = j;
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
-
-	std::vector<std::map<std::int64_t, int>> validN(14);
-	std::set<std::int64_t> targetRange;
-	targetRange.insert(0);
-
-	std::array<std::int32_t, 14> zTops = { 10, 10, 100, 100, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 100, 50 };
-	for (std::int32_t i = 13; i >= 2; i--)
-	{
-		std::set<std::int64_t> nextTargetRange;
-		
-		for (std::int64_t z = 0; z <= zTops[i]; z++)
-		{
-			for (std::int64_t n : {1, 2, 3, 4, 5, 6, 7, 8, 9})
-			{
-				ALU alu([n]() { return n; });
-				alu.RunInstruction(ALU::Instruction::add, ALU::RegisterName::z, z);
-				for (auto& inst : program[i])
-				{
-					alu.RunInstruction(inst);
-				}
-
-				auto zVal = alu.GetValue(ALU::RegisterName::z);
-				if (targetRange.contains(zVal))
-				{
-					nextTargetRange.insert(z);
-
-					validN[i].emplace(z, n);
-				}
-			}
-		}
-		std::cout << "Index: " << i << " z-range: " << *nextTargetRange.begin() << " " << *nextTargetRange.rbegin() << std::endl;
-		std::swap(targetRange, nextTargetRange);
-
-		std::cout << std::endl;
-	}
-
-	////Verify result
-	////testVal = { 9, 9, 9, 9, 4, 9, 9, 9, 9, 9, 9, 9, 9, 9 };
-	std::size_t i = 0;
-	std::int64_t lastZ = 0;
-	auto inputFunc = [&]() -> std::int64_t
-	{
-		if (!validN[i].empty())
-		{
-			assert(validN[i].contains(lastZ));
-			return validN[i][lastZ];
-		}
-
-		return testVal[i];
 	};
 
-	ALU aluVeri(inputFunc);
-	for (i = 0; i < testVal.size(); i++)
+	testToMax(3);
+	testToMax(5);
+	testToMax(9);
+	testToMax(10);
+	testToMax(11);
+	testToMax(12);
+	testToMax(13);
+
+	//Verify result
+	auto inputFunc = [&]() -> std::int64_t
+	{
+		return coefficients[i];
+	};
+
+	ALU aluVeri1(inputFunc);
+	for (i = 0; i < coefficients.size(); i++)
 	{
 		for (auto& inst : program[i])
 		{
-			aluVeri.RunInstruction(inst);
+			aluVeri1.RunInstruction(inst);
 		}
-		lastZ = aluVeri.GetValue(ALU::RegisterName::z);
+		std::cout << aluVeri1.GetValue(ALU::RegisterName::z) << std::endl;
 	}
 
-	assert(aluVeri.GetValue(ALU::RegisterName::z) == 0);
+	assert(aluVeri1.GetValue(ALU::RegisterName::z) == 0);
 
 	std::cout << "Part 1: ";
-	for (int val : testVal)
+	for (int val : coefficients)
 	{
 		std::cout << val;
 	}
 	std::cout << std::endl;
 
 
-	//std::cout << "Part 2: " << count << std::endl;
+	coefficients = { 1, 4, 9, 1, 1, 1, 7, 5, 3, 1, 1, 1, 1, 1 };
+	auto testToMin = [&](int coeff) {
+		std::int64_t minOutput = std::numeric_limits<std::int64_t>::max();
+		for (int nc = 1; nc <= 9; nc++)
+		{
+			ALU alu([&]() -> std::int64_t
+				{
+					if (i == coeff)
+					{
+						return nc;
+					}
+					return coefficients[i];
+				});
+
+			for (i = 0; i <= coeff; i++)
+			{
+				for (auto& inst : program[i])
+				{
+					alu.RunInstruction(inst);
+				}
+			}
+			auto intermediateVal = alu.GetValue(ALU::RegisterName::z);
+			if (intermediateVal < minOutput)
+			{
+				minOutput = intermediateVal;
+				coefficients[coeff] = nc;
+
+			}
+		}
+	};
+	
+	//testToMin(0);
+	//testToMin(1);
+	//testToMin(2);
+	testToMin(3);
+	//testToMin(4);
+	testToMin(5);
+	//testToMin(6);
+	//testToMin(7);
+	//testToMin(8);
+	testToMin(9);
+	testToMin(10);
+	testToMin(11);
+	testToMin(12);
+	testToMin(13);
+
+	//Verify result
+	ALU aluVeri2(inputFunc);
+	for (i = 0; i < coefficients.size(); i++)
+	{
+		for (auto& inst : program[i])
+		{
+			aluVeri2.RunInstruction(inst);
+		}
+		std::cout << aluVeri2.GetValue(ALU::RegisterName::z) << std::endl;
+	}
+
+	//assert(aluVeri2.GetValue(ALU::RegisterName::z) == 0);
+
+	std::cout << "Part 2: ";
+	for (int val : coefficients)
+	{
+		std::cout << val;
+	}
+	std::cout << std::endl;
 }
